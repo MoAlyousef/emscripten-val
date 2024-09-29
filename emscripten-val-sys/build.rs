c2479c3f467f965fda38927068f908f417f54363
Mohammed Alyousef <may642_2000@hotmail.com>
2024-09-30 01:23:03 +0300

    use cc crate

--- emscripten-val-sys/build.rs
+++ emscripten-val-sys/build.rs
@@ -2,28 +2,10 @@ fn main() {
     println!("cargo:rerun-if-changed=emval_support/emval.h");
     println!("cargo:rerun-if-changed=emval_support/emval_support.cpp");
     println!("cargo:rerun-if-changed=emval_support/val_support.cpp");
-    println!("cargo:rerun-if-changed=emval_support/CMakeLists.txt");
-    let emsdk = std::env::var("EMSDK").expect("EMSDK undefined!");
-    let toolchain_file = std::path::PathBuf::from(emsdk)
-        .join("upstream")
-        .join("emscripten")
-        .join("cmake")
-        .join("Modules")
-        .join("Platform")
-        .join("Emscripten.cmake");
-    let mut dst = cmake::Config::new("emval_support");
-    dst.define("CMAKE_TOOLCHAIN_FILE", toolchain_file);
-    dst.profile("Release");
-    dst.build();
-    // cc::Build::new()
-    //     .file("src/emval_support.cpp")
-    //     .cpp(true)
-    //     .compile("emval_support");
-    let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    cc::Build::new()
+        .file("emval_support/emval_support.cpp")
+        .file("emval_support/val_support.cpp")
+        .cpp(true)
+        .compile("emval_support");
     println!("cargo:rustc-link-lib=embind");
-    println!(
-        "cargo:rustc-link-search=native={}",
-        out_dir.join("lib").display()
-    );
-    println!("cargo:rustc-link-lib=static=emval_support");
 }
