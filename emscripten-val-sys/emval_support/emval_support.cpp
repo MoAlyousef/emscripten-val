efb2a6acd88c6ea06d98c3c72b14d8b92cd80dfd
Mohammed Alyousef <may642_2000@hotmail.com>
2024-10-02 23:24:19 +0300

    0.1.7

--- emscripten-val-sys/emval_support/emval_support.cpp
+++ emscripten-val-sys/emval_support/emval_support.cpp
@@ -10,24 +10,32 @@ using namespace emscripten;
 EMSCRIPTEN_BINDINGS(MyBindings) {
     class_<std::function<val()>>("ListenerCallback0")
         .constructor<>()
-        .function("handleEvent", &std::function<val()>::operator());
+        .function("_internal_func_", &std::function<val()>::operator());
     class_<std::function<val(val)>>("ListenerCallback1")
         .constructor<>()
-        .function("handleEvent", &std::function<val(val)>::operator());
+        .function("_internal_func_", &std::function<val(val)>::operator());
     class_<std::function<val(val, val)>>("ListenerCallback2")
         .constructor<>()
-        .function("handleEvent", &std::function<val(val, val)>::operator());
+        .function("_internal_func_", &std::function<val(val, val)>::operator());
     class_<std::function<val(val, val, val)>>("ListenerCallback3")
         .constructor<>()
         .function(
-            "handleEvent", &std::function<val(val, val, val)>::operator()
+            "_internal_func_", &std::function<val(val, val, val)>::operator()
         );
     class_<std::function<val(val, val, val, val)>>("ListenerCallback4")
         .constructor<>()
         .function(
-            "handleEvent", &std::function<val(val, val, val, val)>::operator()
+            "_internal_func_",
+            &std::function<val(val, val, val, val)>::operator()
         );
 };
+
+template <typename... T>
+static val func_to_val(std::function<val(T...)> &&func) {
+    return val(func)["_internal_func_"].call<val>(
+        "bind", emscripten::val(func)
+    );
+}
 
 internal::TYPEID EmvalType = internal::TypeID<val>::get();
 internal::TYPEID BoolType  = internal::TypeID<bool>::get();
@@ -53,7 +61,7 @@ char *_emval_as_str(EM_VAL object) {
 
 void _emval_add_event_listener(EM_VAL em, const char *name, void *data) {
     auto v    = val::take_ownership(em);
-    auto func = val(std::function<val(val)>([=](val ev) -> val {
+    auto func = func_to_val(std::function<val(val)>([=](val ev) -> val {
         return val::take_ownership(
             emscripten_val_rust_caller1(ev.as_handle(), data)
         );
@@ -64,29 +72,30 @@ void _emval_add_event_listener(EM_VAL em, const char *name, void *data) {
 
 EM_VAL _emval_take_fn(unsigned char argcount, void *data) {
     val func;
-
     switch (argcount) {
     case 0:
-        func = val(std::function<val()>([=]() -> val {
+        func = func_to_val(std::function<val()>([=]() -> val {
             return val::take_ownership(emscripten_val_rust_caller0(data));
         }));
         break;
     case 1:
-        func = val(std::function<val(val)>([=](val ev) -> val {
+        func = func_to_val(std::function<val(val)>([=](val ev) -> val {
             return val::take_ownership(
                 emscripten_val_rust_caller1(ev.as_handle(), data)
             );
         }));
         break;
     case 2:
-        func = val(std::function<val(val, val)>([=](val a0, val a1) -> val {
-            return val::take_ownership(emscripten_val_rust_caller2(
-                a0.as_handle(), a1.as_handle(), data
-            ));
-        }));
+        func = func_to_val(
+            std::function<val(val, val)>([=](val a0, val a1) -> val {
+                return val::take_ownership(emscripten_val_rust_caller2(
+                    a0.as_handle(), a1.as_handle(), data
+                ));
+            })
+        );
         break;
     case 3:
-        func = val(std::function<val(val, val, val)>(
+        func = func_to_val(std::function<val(val, val, val)>(
             [=](val a0, val a1, val a2) -> val {
                 return val::take_ownership(emscripten_val_rust_caller3(
                     a0.as_handle(), a1.as_handle(), a2.as_handle(), data
@@ -95,7 +104,7 @@ EM_VAL _emval_take_fn(unsigned char argcount, void *data) {
         ));
         break;
     case 4:
-        func = val(std::function<val(val, val, val, val)>(
+        func = func_to_val(std::function<val(val, val, val, val)>(
             [=](val a0, val a1, val a2, val a3) -> val {
                 return val::take_ownership(emscripten_val_rust_caller4(
                     a0.as_handle(),
@@ -107,7 +116,7 @@ EM_VAL _emval_take_fn(unsigned char argcount, void *data) {
             }
         ));
     default:
-        func = val(std::function<val(val)>([=](val ev) -> val {
+        func = func_to_val(std::function<val(val)>([=](val ev) -> val {
             return val::take_ownership(
                 emscripten_val_rust_caller1(ev.as_handle(), data)
             );
