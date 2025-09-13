3140ea9c9c611ab39065bae8d2cbf5138cf6011c
Antoine Delègue <delegue.antoine@gmail.com>
2025-09-13 21:05:40 +0200

    add function to convert from js string to vec<u8>

--- src/val.rs
+++ src/val.rs
@@ -435,6 +435,15 @@ impl Val {
         }
     }
 
+    pub fn as_bytes(&self) -> Vec<u8> {
+        unsafe {
+            let ptr = _emval_as_str(self.handle);
+            let ret = CStr::from_ptr(ptr).to_bytes().to_vec();
+            free(ptr as _);
+            ret
+        }
+    }
+
     /// Convert a javascript Array to a Rust Vec
     pub fn to_vec<T: JsType>(&self) -> Vec<T> {
         let len = self.get(&"length").as_::<u32>();
